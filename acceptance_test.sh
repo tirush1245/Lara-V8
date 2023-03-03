5a7355cfad23e5b1a1ab2e1e682c778553dfbd9d
tirush1245 <108980519+tirush1245@users.noreply.github.com>
2023-03-03 20:35:02 +0530

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-res=$(curl -s -w "%{http_code}" $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' laravel8cd)/public/sum/4/2)
+res=$(curl -s -w "%{http_code}" $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' lara8)/public/sum/4/2)
 body=${res::-3}
 if [ $body != "6" ]; then
   echo "Error"
