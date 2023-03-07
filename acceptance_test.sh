2100c59557004c67942e419b835e824c509f2e73
tirush1245 <108980519+tirush1245@users.noreply.github.com>
2023-03-07 14:53:20 +0530

    Delete acceptance_test.sh

--- acceptance_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-res=$(curl -s -w "%{http_code}" $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' lara8)/public/sum/4/2)
-body=${res::-3}
-if [ $body != "6" ]; then
-  echo "Error"
-fi
